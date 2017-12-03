077143c17f3a716e2efb4d0daab8cb0ed27059a7
hibingoo <33690138+hibingoo@users.noreply.github.com>
2017-12-03 10:55:47 +0800

    制作入口函数并将按钮与响应函数链接
    在按钮的属性检查器里添加button与响应函数连接

--- bycw_client/assets/scripts/game/talkroom.js
+++ bycw_client/assets/scripts/game/talkroom.js
@@ -33,6 +33,11 @@ cc.Class({
         //    readonly: false,    // optional, default is false
         // },
         // ...
+        input: {
+            default: null,
+            type: cc.EditBox,
+        },
+        //定义一个输入框属性，这样就可以把做好的输入框拖到属性检查器的这个入口上
     },
 
     // use this for initialization
@@ -66,6 +71,31 @@ cc.Class({
     },
 
     start: function() {
+
+    },
+    
+    //链接聊天室按钮的入口函数
+    connect_to_talkroom: function(){
+       console.log("connect_to_talkroom");
+    },
+    //退出聊天室按钮的入口函数
+    disconnect_to_talkroom: function(){
+       console.log("disconnect_to_talkroom");
+    },
+    //发送信息按钮的入口函数
+    send_msg_to_talkroom: function(){
+        //获得链接到该脚本上文字输入框中的文字
+        var str = this.input.string;
+        //如果没有文字或者小于0，则返回
+        if (!str || str.length <=0){
+            return;
+        }
+        //有的话就打印该字符
+        console.log(str);
+        //打印后置空
+        this.input.string="";
+
+
 
     },
     // called every frame, uncomment this function to activate update callback
